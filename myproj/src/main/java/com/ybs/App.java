a50cd3c151400e5b47f270d25cff1a600754df57
Your Name <you@example.com>
2021-12-07 20:47:08 +0530

    added java proj

---
+++ myproj/src/main/java/com/ybs/App.java
@@ -0,0 +1,13 @@
+package com.ybs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
